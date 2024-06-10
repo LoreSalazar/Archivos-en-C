5b6d7dafa8bfd9ab4361f35cfdab7a7513735d84
LSL <lorena.salazarlara@hotmail.com>
2024-06-09 23:03:44 -0600

    Comienzo del proyecto

---
+++ archivos.c
@@ -0,0 +1,10 @@
+/** Haga un programa que almacene en diferentes vectores, los nombres, generos y edades de un grupo de n personas. El programa debe obtener:
+
+a) El número de personas de genero masculino.
+b) El número de personas de género femenino que son mayores de edad
+c) El promedio de edad de los hombres
+d) El nombre de la mujer más joven 
+
+(pág 472)**/
+
+
